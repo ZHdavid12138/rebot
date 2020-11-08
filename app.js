09bcb8b51a6630139c493e35986b9c6d8a849c80
ZHdavid12138 <68285771+ZHdavid12138@users.noreply.github.com>
2020-11-08 14:19:21 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const fs = require('fs')
+const { rejects } = require('assert');
+const fs = require('fs');
+const { resolve } = require('path');
 const TeleBot = require('telebot')
 const { Writefile, Readfile } = require('./readfile')
 const bot = new TeleBot({
@@ -16,29 +18,30 @@ const AmindN = '' //管理员用户名
 
 //同步读取配置
 var info = JSON.parse(fs.readFileSync('./user.json').toString())
-for(var item in info)
-{
+var AllsetI = {}
+
+
+//监听文本改变或状态改变
+for (var item in info) {
     info[item].statusc = info[item].status
-    Object.defineProperty(info[item], "status",{
-        get : function(){
+    Object.defineProperty(info[item], "status", {
+        get: function () {
             return this.statusc
         },
-        set : function(val){
-            if(this.statusc == val) return//返回无用参数
+        set: function (val) {
+            if (this.statusc == val) return//返回无用参数
             this.statusc = val
-            if(val == 1)
-            {
+            if (val == 1) {
                 StartA()
             }
         }
     })
-    function StartA()
-    {
-        console.log("jiso")
+    function StartA() {
         var setI = setInterval(() => {
-            if(info[item].status == 0) return clearInterval(setI)//循环前判断状态
+            if (info[item].status == 0) return clearInterval(setI)//循环前判断状态
             bot.sendMessage(info[item].id, info[item].text)
         }, info[item].setInterval * 1000)
+        AllsetI[item] = setI
     }
     StartA()
 }
@@ -60,7 +63,7 @@ function Startplay(msg) {
         }
         Writefile('./user.json', JSON.stringify(info)).then(() => {
             msg.reply.text('已开启全部')
-        },(err) =>{ msg.reply.text('写入配置失败') })
+        }, (err) => { msg.reply.text('写入配置失败') })
     }
 }
 
@@ -71,11 +74,15 @@ function Stopplay(msg) {
             if (info.hasOwnProperty(key)) {
                 const element = info[key];
                 element.status = 0
+                if(AllsetI[key]) {
+                    clearInterval(AllsetI[key])
+                    delete AllsetI[key]
+                }
             }
         }
         Writefile('./user.json', JSON.stringify(info)).then(() => {
             msg.reply.text('已关闭全部')
-        },(err) =>{ msg.reply.text('写入配置失败') })
+        }, (err) => { msg.reply.text('写入配置失败') })
     }
 }
 
@@ -83,46 +90,81 @@ function SetGroupInfo(msg) {
     //添加更新群组配置表
     let from = msg.chat
     if (from.username === AmindN && from.type === 'private') {
-        try {
-            let arry = msg.text.split(' ')
-            let json = {}
-            arry[2] = Number(arry[2])
-            if(arry[2] < 5 || arry[2] > 36000)
-            {
-                return msg.reply.text('间隔时间太短或太长')
-            } else if (arry[3].length > 1000 || arry[3] == ' ' || arry[3].length == 0)
-                return msg.reply.text('文本太长或没有设置文本')
-            Readfile('./user.json').then((res) => {
-                json = res
-                json[arry[1]] = {
-                    setInterval: arry[2],
-                    text: arry[3],
-                    status: arry[4] ? 1 : 0
+        let arry = msg.text.split(' ')
+        arry[2] = Number(arry[2])
+        arry[4] = Number(arry[4] == '' ? 0 : arry[4])
+        if (arry[2] < 5 || arry[2] > 36000) {
+            return msg.reply.text('间隔时间太短或太长')
+        } else if (arry[3].length > 1000 || arry[3] == ' ' || arry[3].length == 0)
+            return msg.reply.text('文本太长或没有设置文本')
+
+        if (info[arry[1]] === undefined) {
+            msg.reply.text('正在等待获取id,请返回群组发送任意消息')
+            bot.on("text", (meassage) => {
+                if (meassage.chat.title == arry[1]) {
+                    var id = meassage.chat.id
+                    arry[0] = id
+                    Updateinfo(arry).then(() => {
+                        msg.reply.text('添加成功')
+                        return bot.cleanEvent('text')
+                    }, () => {
+                        msg.reply.text('写入文件失败')
+                        return bot.cleanEvent('text')
+                    })
                 }
-                Writefile('./user.json', JSON.stringify(json)).then(() => {
-                    msg.reply.text('添加成功')
-                    info = json
-                })
             })
-        } catch (error) {
-            return
+        }
+        else {
+            arry[0] = false
+            Updateinfo(arry).then(() => {
+                return msg.reply.text('修改成功')
+            }, () => {
+                return msg.reply.text('写入文件失败')
+            })
         }
     }
     else return
 }
 
+function Updateinfo(arry) {
+    return new Promise((resolve, rejects) => {
+        Readfile('./user.json').then((res) => {
+            if(res[arry[1]].status != arry[4] && res[arry[1]].status == 1)
+            {
+                if(AllsetI[key]) {
+                    clearInterval(AllsetI[key])
+                    delete AllsetI[key]
+                }
+            }
+            let json = res
+            json[arry[1]] = {
+                id: arry[0] || res[arry[1]].id,
+                setInterval: arry[2],
+                text: arry[3],
+                status: arry[4]
+            }
+            Writefile('./user.json', JSON.stringify(json)).then(() => {
+                info = json
+                resolve('')
+            }, (err) => {
+                rejects('')
+            })
+        })
+    })
+}
+
+
+
 function RemoveGroupUser(msg) {
     //删除用户配置
     let from = msg.chat
-    if (from.username === AmindN && from.type === 'private') 
-    {
+    if (from.username === AmindN && from.type === 'private') {
         try {
             let arry = msg.text.split(' ')
             let json = {}
             Readfile('./user.json').then((res) => {
                 json = res
-                if(json[arry[1]] !== undefined)
-                {
+                if (json[arry[1]] !== undefined) {
                     delete json[arry[1]]
                     Writefile('./user.json', JSON.stringify(json)).then(() => {
                         msg.reply.text('添加成功')
@@ -137,8 +179,7 @@ function RemoveGroupUser(msg) {
     }
     else return
 }
-function ShowGrouplist(msg)
-{
+function ShowGrouplist(msg) {
 
 }
 //搜索功能
